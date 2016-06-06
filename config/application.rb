b102ae573ee9b6889747d4b267a2ed7c6147af14
Philip Castiglione <philipcastiglione@gmail.com>
2016-06-06 14:57:44 +1000

    added react-rails config

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module NmVino
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.react.variant = :development
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.react.variant = :production
 end
