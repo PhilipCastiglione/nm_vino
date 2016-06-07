36f4b534300553061b168b9f2c4bbfc119ad11cd
Philip Castiglione <philipcastiglione@gmail.com>
2016-06-07 16:45:34 +1000

    winding back es6 use

--- app/assets/javascripts/scores.js
+++ app/assets/javascripts/scores.js
@@ -1,14 +1,14 @@
 function showScores() {
   var $tables = $('table');
   if ($tables.length > 0) {
-    $.each($tables, ((i, table) => {
+    $.each($tables, (function(i, table) {
       var scores = JSON.parse(localStorage.getItem('scores:' + table.dataset.measure));
-      $.each(scores, ((key, val) => {
+      $.each(scores, (function(key, val) {
         $tr = $('<tr>');
         $tr.append($('<td>').text(val['name']));
         $tr.append($('<td>').text((new Date(val['date'])).toLocaleString()));
-        $.each(val['report'], ((mc, metrics) => {
-          $.each(metrics, ((metric, metric_score) => {
+        $.each(val['report'], (function(mc, metrics) {
+          $.each(metrics, (function(metric, metric_score) {
             if (metric === 'total') {
               $tr.append($('<td>').text(metric_score));
             }
